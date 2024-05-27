16d626c6b176cbbd9d6d9c61545b5b5dd830f187
tzuchaedahy <viniciuslovestwice@gmail.com>
2024-05-27 15:26:33 -0300

    second exercise (scanner, loop and conditional)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,12 @@
 package org.example;
 
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        firstExercise();
+//        firstExercise();
+        secondExercise();
     }
 
     private static String capitalizeFirstLetter(String str) {
@@ -39,5 +41,35 @@ public class Main {
         System.out.printf("Rounded (three decimal places): %.3f\n", measure);
         Locale.setDefault(Locale.US);
         System.out.printf("US decimal point: %.3f\n", measure);
+    }
+
+    private static float askUserForCelsiusTemperature(Scanner scanner) {
+        System.out.println("Enter temperature in celsius: ");
+
+        return scanner.nextFloat();
+    }
+
+    private static float convertCelsiusToFahrenheit(float celsiusTemperature) {
+        return celsiusTemperature * 9 / 5 + 32;
+    }
+
+    private static boolean askUserForRepeatingProcess(Scanner scanner) {
+        System.out.println("You wish to convert again? (y/n): ");
+        char response = scanner.next().toLowerCase().charAt(0);
+
+        return response == 'y';
+    }
+
+    private static void secondExercise() {
+        Scanner scanner = new Scanner(System.in);
+        boolean repeat = true;
+
+        do {
+            float fahrenheit = convertCelsiusToFahrenheit(askUserForCelsiusTemperature(scanner));
+            System.out.printf("Temperature in fahrenheit: %.1f\n", fahrenheit);
+            repeat = askUserForRepeatingProcess(scanner);
+        } while (repeat);
+
+        scanner.close();
     }
 }
